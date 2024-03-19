08db53ecfe2b94631f90895827ef672b9c01ee70
DoanVanDuong <139871844+DoanVanDuong@users.noreply.github.com>
2024-03-19 15:29:33 +0700

    sửa db và qldh

--- app/src/main/java/com/example/duan1_nhom5/ManHinhChao.java
+++ app/src/main/java/com/example/duan1_nhom5/ManHinhChao.java
@@ -12,25 +12,11 @@ import com.airbnb.lottie.LottieAnimationView;
 
 
 public class ManHinhChao extends AppCompatActivity {
-    TextView appname;
-    LottieAnimationView lottie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manhinhchao);
-
-        appname = findViewById(R.id.appname);
-        lottie = findViewById(R.id.lottie);
-
-
-        String animationUrl = "https://lottie.host/67bfabfe-8b9c-4032-a8c8-d109bb7ffdf4/RZKyUmc9OY.json";
-        lottie.setAnimationFromUrl(animationUrl);
-        lottie.playAnimation();
-
-        appname.animate().translationY(-1400).setDuration(2700).setStartDelay(0);
-        lottie.animate().translationY(2000).setDuration(2700).setStartDelay(2900);
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
--- app/src/main/java/com/example/duan1_nhom5/dao/DonHangChiTietDao.java
+++ app/src/main/java/com/example/duan1_nhom5/dao/DonHangChiTietDao.java
@@ -1,2 +1,51 @@
-package com.example.duan1_nhom5.dao;public class DonHangChiTietDao {
+package com.example.duan1_nhom5.dao;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.duan1_nhom5.DBHelper.db;
+import com.example.duan1_nhom5.model.DonHangChiTiet;
+
+import java.util.ArrayList;
+
+public class DonHangChiTietDao {
+    private SQLiteDatabase db;
+    private SQLiteOpenHelper dbHelper;
+
+    public DonHangChiTietDao(Context context) {
+        dbHelper = new db(context);
+    }
+    public ArrayList<DonHangChiTiet> getList() {
+        ArrayList<DonHangChiTiet> chiTietDonHangList = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        // Thực hiện câu truy vấn join
+        String query = "SELECT ChiTietDonHang.*, SanPham.ten AS tenSanPham, SanPham.gia_tien, " +
+                "(SELECT gia_tri_thuoc_tinh FROM ThuocTinhSanPham WHERE id_san_pham = SanPham.id AND id_thuoc_tinh = (SELECT id FROM ThuocTinh WHERE ten = 'Màu sắc')) AS mauSac, " +
+                "(SELECT gia_tri_thuoc_tinh FROM ThuocTinhSanPham WHERE id_san_pham = SanPham.id AND id_thuoc_tinh = (SELECT id FROM ThuocTinh WHERE ten = 'Kích thước')) AS kichThuoc " +
+                "FROM ChiTietDonHang " +
+                "INNER JOIN SanPham ON ChiTietDonHang.id_san_pham = SanPham.id " +
+                "INNER JOIN ThuocTinhSanPham ON ChiTietDonHang.id_san_pham = ThuocTinhSanPham.id_san_pham";
+        Cursor cursor = db.rawQuery(query, null);
+
+
+        if (cursor.moveToFirst()) {
+            do {
+                DonHangChiTiet chiTietDonHang = new DonHangChiTiet();
+                chiTietDonHang.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                chiTietDonHang.setIdDH(cursor.getInt(cursor.getColumnIndex("id_don_hang")));
+                chiTietDonHang.setIdSP(cursor.getInt(cursor.getColumnIndex("id_san_pham")));
+                chiTietDonHang.setSoLuong(cursor.getInt(cursor.getColumnIndex("so_luong")));
+                chiTietDonHang.setGia(cursor.getInt(cursor.getColumnIndex("gia_tien")));
+                chiTietDonHang.setMau(cursor.getString(cursor.getColumnIndex("mauSac")));
+                chiTietDonHang.setKichThuoc(cursor.getString(cursor.getColumnIndex("kichThuoc")));
+                chiTietDonHangList.add(chiTietDonHang);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return chiTietDonHangList;
+    }
+
 }
