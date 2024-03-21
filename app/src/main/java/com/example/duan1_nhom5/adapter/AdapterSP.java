6e6587abb5e1fd7450c056150f96c142b5199a03
vinh04042004@gmail.com <vinh04012004@gmail.com>
2024-03-21 16:13:48 +0700

    Chức năng sản phầm và quàn lý sản phẩm

---
+++ app/src/main/java/com/example/duan1_nhom5/adapter/AdapterSP.java
@@ -0,0 +1,71 @@
+package com.example.duan1_nhom5.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.duan1_nhom5.R;
+import com.example.duan1_nhom5.dao.SanPhamDao;
+import com.example.duan1_nhom5.model.SanPham;
+
+import java.util.ArrayList;
+
+public class AdapterSP extends RecyclerView.Adapter<AdapterSP.ViewHolder> {
+    private Context context;
+    private ArrayList<SanPham> list;
+    private SanPhamDao sanPhamDao;
+
+
+    public AdapterSP(Context context, ArrayList<SanPham> list) {
+        this.context = context;
+        this.list = list;
+        sanPhamDao = new SanPhamDao(context);
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_qlysp, parent, false);
+        return new ViewHolder(view);    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        SanPham sanPham = list.get(position);
+        holder.textViewName.setText(String.valueOf(sanPham.getName()));
+        holder.textViewQuantity.setText(String.valueOf(sanPham.getQuantity()));
+        holder.textViewPrice.setText(String.valueOf(sanPham.getPrice()));
+        holder.textViewBrand.setText(String.valueOf(sanPham.getBrand()));
+        holder.textViewSize.setText(String.valueOf(sanPham.getSize()));
+        holder.textViewColor.setText(String.valueOf(sanPham.getColor()));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewName, textViewColor, textViewSize, textViewBrand, textViewPrice, textViewQuantity;
+
+        ImageView imggiohang;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textViewName = itemView.findViewById(R.id.textViewName);
+            textViewColor = itemView.findViewById(R.id.textViewColor);
+            textViewSize = itemView.findViewById(R.id.textViewSize);
+            textViewBrand = itemView.findViewById(R.id.textViewBrand);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            textViewQuantity = itemView.findViewById(R.id.textViewQuantity);
+            imggiohang = itemView.findViewById(R.id.imggiohang);
+        }
+}
+}
--- app/src/main/java/com/example/duan1_nhom5/model/SanPham.java
+++ app/src/main/java/com/example/duan1_nhom5/model/SanPham.java
@@ -1,4 +1,88 @@
 package com.example.duan1_nhom5.model;
 public class SanPham {
+    private int id;
+    private String name;
+    private int quantity;
+    private int price;
+    private String color;
+    private String size;
+    private String brand;
 
+    public SanPham() {
+    }
+
+    public SanPham(int id, String name, int quantity, int price, String color, String size, String brand) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.color = color;
+        this.size = size;
+        this.brand = brand;
+    }
+
+    public SanPham(String name, int quantity, int price, String color, String size, String brand) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.color = color;
+        this.size = size;
+        this.brand = brand;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
