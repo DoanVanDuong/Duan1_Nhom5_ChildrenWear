f5f0d07a923ee46fad1a04a3d985d6f95f46e612
VDTuan0108 <134400283+VDTuan0108@users.noreply.github.com>
2024-04-10 09:41:35 +0700

    GioHang

--- app/src/main/java/com/example/duan1_nhom5/adapter/AdapterSP.java
+++ app/src/main/java/com/example/duan1_nhom5/adapter/AdapterSP.java
@@ -93,7 +93,7 @@ public class AdapterSP extends RecyclerView.Adapter<AdapterSP.ViewHolder> implem
         return list.size();
     }
 
-    @Override
+@Override
     public Filter getFilter() {
         return new Filter() {
             @Override
